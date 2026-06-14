c877d53a14c06007e48b58c8de557e2d1607018a
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-06-14 20:57:21 +0300

    Full-featured Stone-Paper-Scissor game.

--- Course05-Project 1 StonePaperScissor Game/Course05-Project 1 StonePaperScissor Game.cpp
+++ Course05-Project 1 StonePaperScissor Game/Course05-Project 1 StonePaperScissor Game.cpp
@@ -1,20 +1,201 @@
-// Course05-Project 1 StonePaperScissor Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <string>
 
-int main()
+using namespace std;
+//Project 1 StonePaperScissor Game
+enum enPlayer { Player = 1, Computer = 2 };
+enum enRoundResult { player = 1, computer = 2, draw = 3 };
+enum enChoice { Stone = 1, Paper = 2, Scissor = 3 };
+
+int RandomNumber(int From, int To)
 {
-    std::cout << "Hello World!\n";
+    int random = rand() % (To - From + 1) + From;
+    return random;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    return Number;
+}
+
+int ReadNumberInRange(int From, int To)
+{
+    int Number = 0;
+    do
+    {
+        cin >> Number;
+        if (Number < From || Number > To)
+            cout << "\nInvalid Input!\n";
+    } while (Number < From || Number > To);
+
+    return Number;
+}
+int ReadNumberInRange(int From, int To, string Message, string InValidInputMessage = "")
+{
+    int Number = 0;
+    do
+    {
+        cout << Message;
+        cin >> Number;
+        if (Number < From || Number > To)
+        {
+            cout << "\nInvalid Input!\n";
+            cout << InValidInputMessage;
+        }
+    } while (Number < From || Number > To);
+
+    return Number;
+}
+short playerWonCount = 0, computerWonCount = 0, drawCount = 0;
+enRoundResult WWhoonTheRound(enChoice PlayerChoice, enChoice ComputerChoice)
+{
+    if (PlayerChoice == ComputerChoice)
+    {
+        drawCount++;
+        system("color 3F"); //Blue
+        return   enRoundResult::draw;
+    }
+    else if (PlayerChoice == enChoice::Paper && ComputerChoice == enChoice::Stone
+        || (PlayerChoice == enChoice::Scissor && ComputerChoice == enChoice::Paper)
+        || (PlayerChoice == enChoice::Stone && ComputerChoice == enChoice::Scissor))
+    {
+        playerWonCount++;
+        system("color 2F"); //Green
+        return   enRoundResult::player;
+    }
+    else
+    {
+        computerWonCount++;
+        system("color 4F"); //Red
+        return   enRoundResult::computer;
+    }
+}
+
+string ConvertEnumNumberToText(enChoice choice)
+{
+    return choice == enChoice::Paper ? "Paper" : (choice == enChoice::Scissor) ? "Scissor" : "Stone";
+}
+string ConvertEnumNumberToText(enPlayer Player)
+{
+    return Player == enPlayer::Player ? "Player" : "Computer";
+}
+string ConvertEnumNumberToText(enRoundResult RoundResult)
+{
+    return RoundResult == enRoundResult::computer ? "\aComputer" : (RoundResult == enRoundResult::player) ? "Player" : "Draw";
+}
+
+void PrintRoundResult(enChoice PlayerChoice, enChoice ComputerChoice, short RoundNumber, enRoundResult WWhoinTheRound)
+{
+    cout << "\n----------------Round[" << RoundNumber << "]----------------\n";
+    cout << "Player Choice   : " << ConvertEnumNumberToText(PlayerChoice) << endl;
+    cout << "Computer Choice : " << ConvertEnumNumberToText(ComputerChoice) << endl;
+    cout << "Round Winner    : [" << ConvertEnumNumberToText(WWhoinTheRound) << "]." << endl;
+    cout << "----------------------------------------\n";
+}
+
+void RoundBegins(short WhoManyRounds)
+{
+    enChoice PlayerChoice, ComputerChoice;
+
+
+
+    playerWonCount = 0, computerWonCount = 0, drawCount = 0;
+    for (short i = 1; i <= WhoManyRounds; i++)
+    {
+        cout << "\nRound [" << i << "] begins:\n";
+
+        cout << "\nYour Choice : [1]:Stone , [2]:Paper , [3]:Scissor >> ";
+        PlayerChoice = (enChoice)ReadNumberInRange(1, 3, "", "\nYour Choice : [1]:Stone , [2]:Paper , [3]:Scissor >> ");
+        ComputerChoice = (enChoice)RandomNumber(1, 3);
+        PrintRoundResult(PlayerChoice, ComputerChoice, i, WWhoonTheRound(PlayerChoice, ComputerChoice));
+    }
+
+}
+string Tabs(short NumberOfTabs = 3)
+{
+    string tabs = "";
+    for (short i = 1; i <= NumberOfTabs; i++)
+        tabs += "\t";
+    return tabs;
+}
+void SWhoGameOverScreen()
+{
+    cout << Tabs() << "--------------------------------------------------------------\n";
+    cout << Tabs(6) << "G a m e  O v e r \n";
+    cout << Tabs() << "--------------------------------------------------------------\n";
+}
+void DisplayWinnerColor()
+{
+    playerWonCount == computerWonCount ? system("color 3F") : playerWonCount > computerWonCount ? system("color 2F") : system("color 4F");
+}
+
+string WWhoonTheGame()
+{
+    return    playerWonCount == computerWonCount ? "No Winner" : playerWonCount > computerWonCount ? "Player Wins" : "Computer Wins";
+}
+void DisplayBackgroundColorBack(HANDLE hConsole)
+{
+    playerWonCount == computerWonCount ? SetConsoleTextAttribute(hConsole, 63) : playerWonCount > computerWonCount ? SetConsoleTextAttribute(hConsole, 47) : SetConsoleTextAttribute(hConsole, 79);
+}
+void GameResults(short WhoManyRounds)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    DisplayWinnerColor();
+    cout << Tabs() << "---------------[Game Results]---------------------------------" << endl;
+
+    cout << Tabs() << "Game Rounds        : " << to_string(WhoManyRounds) << "\n";
+
+    SetConsoleTextAttribute(hConsole, 160);
+    cout << Tabs() << "Player won times   : " << playerWonCount;
+    DisplayBackgroundColorBack(hConsole);
+    cout << "\n";
+
+    SetConsoleTextAttribute(hConsole, 192);
+    cout << Tabs() << "Computer won times : " << computerWonCount;
+    DisplayBackgroundColorBack(hConsole);
+    cout << "\n";
+
+    SetConsoleTextAttribute(hConsole, 176);
+    cout << Tabs() << "Draw times         : " << drawCount;
+    DisplayBackgroundColorBack(hConsole);
+    cout << "\n";
+
+
+    cout << Tabs() << "Final Winner       : [" << WWhoonTheGame() << "]." << endl;
+    cout << Tabs() << "--------------------------------------------------------------\n";
+}
+void StartGame()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    char PlayAgain = 'y';
+    do
+    {
+        system("cls");
+        system("color 0F");
+
+        short WhoManyRounds = (short)ReadNumberInRange(1, 10, "Who Many Rounds 1 to 10 ? : ");
+        RoundBegins(WhoManyRounds);
+        SWhoGameOverScreen();
+        GameResults(WhoManyRounds);
+
+        cout << "\nDo You want to play again ? [Y/N] : ";
+        cin >> PlayAgain;
+    } while (PlayAgain == 'y' || PlayAgain == 'Y');
+
+}
+int main() {
+    srand((unsigned)time(NULL));
+
+    StartGame();
+    return 0;
+}
